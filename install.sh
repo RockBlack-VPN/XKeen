6bf75beaf4615491a92c98a8cb145eba47c686df
RockBlack <98517320+RockBlack-VPN@users.noreply.github.com>
2026-01-21 15:23:21 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 opkg install tar
-curl -s -L https://github.com/RockBlack-VPN/XKeen/releases/latest/download/xkeen.tar.gz --output xkeen.tar && tar -xvf xkeen.tar -C /opt/sbin --overwrite > /dev/null && rm xkeen.tar.gz
+curl -s -L https://github.com/RockBlack-VPN/XKeen/releases/latest/download/xkeen.tar.gz --output xkeen.tar && tar -xvf xkeen.tar.gz -C /opt/sbin --overwrite > /dev/null && rm xkeen.tar.gz
 xkeen -i
