d2e0f9ecaaabe549d1c05e76e1c7e778ea1dd4e2
RockBlack <98517320+RockBlack-VPN@users.noreply.github.com>
2026-01-22 23:01:46 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,14 @@
-opkg install tar
-curl -s -L https://github.com/RockBlack-VPN/XKeen/releases/latest/download/xkeen.tar.gz --output xkeen.tar.gz && tar -xvf xkeen.tar.gz -C /opt/sbin --overwrite > /dev/null && rm xkeen.tar.gz
+#!/bin/sh
+
+url="https://github.com/RockBlack-VPN/XKeen/releases/latest/download/xkeen.tar.gz"
+if ! curl -OL --connect-timeout 10 -m 60 "$url"; then
+    if ! curl -OL --connect-timeout 10 -m 60 "https://gh-proxy.com/$url"; then
+        if ! curl -OL --connect-timeout 10 -m 60 "https://ghfast.top/$url"; then
+            echo "Ошибка: не удалось загрузить xkeen.tar.gz"
+            exit 1
+        fi
+    fi
+fi
+
+tar -xvzf xkeen.tar.gz -C /opt/sbin > /dev/null && rm xkeen.tar.gz
 xkeen -i
